d372c914299969ef6315dcb3e53b8f97d8e5d938
Isaac <isaac.vock@yale.edu>
2021-10-26 10:46:36 -0400

    Updated documentation

--- R/Fast_analysis.R
+++ R/Fast_analysis.R
@@ -171,6 +171,9 @@ cBtofast <- function(cB_raw,
 #' @param MLE Logical; if TRUE then replicate logit(fn) is estimated using maximum likelihood; if FALSE more conservative Bayesian hypothesis testing is used
 #' @param lower Lower bound for MLE with L-BFGS-B algorithm
 #' @param upper Upper bound for MLE with L-BFGS-B algorithm
+#' @param mut_reg If MLE has instabilities, empircal mut rate will be used to estimate fn, multiplying pnew by 1+mut_reg and pold by 1-mut_reg to regularize fn
+#' @param p_mean Mean of normal distribution used as prior penalty in MLE of logit(fn)
+#' @param p_sd Standard deviation of normal distribution used as prior peanlty in MLE of logit(fn)
 #' @return List with dataframes providing information about replicate-specific and pooled analysis results. The output includes:
 #' \itemize{
 #'  \item Fn_Estimates; dataframe with estimates for the fraction new and fraction new uncertainty for each feature in each replicate.
